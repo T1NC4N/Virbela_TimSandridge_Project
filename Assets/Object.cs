a0530dfeec0f7cc3097edb83f3c6ef483119e9bf
T1NC4N <timothy.sandridge@snhu.edu>
2022-05-12 16:49:29 -0400

    Initial commit

---
+++ Assets/Object.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Object : MonoBehaviour
+{
+
+    [SerializeField]
+    private Color startColor;
+
+    [SerializeField]
+    private Color highColor;
+
+    private bool highlight;
+    public bool highLight
+    {
+        get { return highlight; }
+        set {
+            highlight = value;
+            if(value)
+            {
+                SetHighColor();
+            }
+            else
+            {
+                UnHighLight();
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    virtual protected void Start()
+    {
+        GetComponent<Renderer>().material.SetColor("_Color", startColor);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    virtual protected void SetHighColor()
+    {
+        GetComponent<Renderer>().material.SetColor("_Color", highColor);
+    }
+
+    virtual protected void UnHighLight()
+    {
+        GetComponent<Renderer>().material.SetColor("_Color", startColor);
+    }
+
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Player : MonoBehaviour
+{
+    string[] tags;
+    List<GameObject> gameObjects = new List<GameObject>();
+
+    GameObject closestGO, prevGo;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tags = new string[] { "Item", "Bot" };
+        foreach (GameObject go in GameObject.FindObjectsOfType(typeof(GameObject)))
+        {
+            if (tags.Contains(go.tag))
+                gameObjects.Add(go);
+        }
+
+        prevGo = closestGO = GetClosestObject().gameObject;
+        SetClosestColor(true);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        SetClosestColor();
+    }
+
+    Transform GetClosestObject()
+    {
+        Transform tMin = null;
+        float minDist = Mathf.Infinity;
+        Vector3 currentPos = transform.position;
+        foreach(GameObject t in gameObjects)
+        {
+            float dist = Vector3.Distance(t.transform.position, currentPos);
+            if (dist < minDist)
+            {
+                tMin = t.transform;
+                minDist = dist;
+            }
+        }
+        return tMin;
+    }
+
+    void SetClosestColor()
+    {
+        closestGO = GetClosestObject().gameObject;
+
+        if (prevGo != closestGO)
+        {
+            closestGO.GetComponent<Object>().highLight = true;
+            prevGo.GetComponent<Object>().highLight = false;
+            prevGo = closestGO;
+        }
+
+    }
+    
+    void SetClosestColor(bool force)
+    {
+        if (force)
+        {
+            closestGO = GetClosestObject().gameObject;
+            closestGO.GetComponent<Object>().highLight = true;
+        }
+    }
+}
